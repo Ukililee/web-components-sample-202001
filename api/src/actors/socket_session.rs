8b17720170350070dc099a713cfefc0e6ac9a42b
Ukililee <107114624+Ukililee@users.noreply.github.com>
2021-04-27 10:57:36 +0000

    protocol update

--- api/src/actors/socket_session.rs
+++ api/src/actors/socket_session.rs
@@ -6,4 +6,5 @@ use crate::messages::{Connected, SendClientMessage, UpdateUserSubscriptions};
 use super::user_store::UserStore;
 
 /// Actor for handling Websocket Connection,
-/// created on each User connec
+/// created on each User connection
+/// holds th

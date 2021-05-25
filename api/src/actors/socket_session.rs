62f82b589f2db0ede565a2f76579860859a98219
Ukililee <107114624+Ukililee@users.noreply.github.com>
2021-05-25 10:00:31 +0000

    Clang-format.

--- api/src/actors/socket_session.rs
+++ api/src/actors/socket_session.rs
@@ -43,4 +43,4 @@ impl Handler<SendClientMessage> for SocketSession {
 /// WebSocket message handler
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for SocketSession {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-      
+        let msg = match m

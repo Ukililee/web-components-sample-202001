22882f9fc1f30fced7daf6e9af9027147acdccc8
Ukililee <107114624+Ukililee@users.noreply.github.com>
2021-06-11 21:06:15 +0000

    Backup.

--- api/src/actors/socket_session.rs
+++ api/src/actors/socket_session.rs
@@ -64,4 +64,5 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for SocketSession {
                                 subscriptions,
                                 user_id: self.user_id,
                             });
-                   
+                        }
+        

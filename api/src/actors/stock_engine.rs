144350425bece27bbc407254d6ba17cd9b155cb9
Ukililee <107114624+Ukililee@users.noreply.github.com>
2021-06-24 19:13:55 +0000

    0.3.0 Changelog

--- api/src/actors/stock_engine.rs
+++ api/src/actors/stock_engine.rs
@@ -12,4 +12,4 @@ const TICK_INTERVAL: u64 = 1;
 
 /// Stock Engine
 /// engine that generates ticks and informs UserStore of Stock Updates
-/// this engine is 
+/// this engine is the only place from

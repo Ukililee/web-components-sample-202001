a0386b0fc2597234cbce7ec80f4e71bb556314a4
Ukililee <107114624+Ukililee@users.noreply.github.com>
2021-06-25 14:10:44 +0000

    merge update

--- api/src/actors/stock_engine.rs
+++ api/src/actors/stock_engine.rs
@@ -12,4 +12,4 @@ const TICK_INTERVAL: u64 = 1;
 
 /// Stock Engine
 /// engine that generates ticks and informs UserStore of Stock Updates
-/// this engine is the only place from
+/// this engine is the only place from where we are updat

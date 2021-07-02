e0db8bb438acc6bae9af4243ed60e07d49966da2
Ukililee <107114624+Ukililee@users.noreply.github.com>
2021-07-02 13:39:56 +0000

    Update shell.md

--- api/src/actors/stock_engine.rs
+++ api/src/actors/stock_engine.rs
@@ -21,4 +21,4 @@ pub(crate) struct StockEngine {
 impl Actor for StockEngine {
     type Context = Context<Self>;
 
-    /// once started, perform ticking and 
+    /// once started, perform ticking and update of stoc

c37854611b29ac95edf10cf37f538853da197613
Ukililee <107114624+Ukililee@users.noreply.github.com>
2021-07-04 21:05:15 +0000

    Update copyright years

--- api/src/actors/stock_engine.rs
+++ api/src/actors/stock_engine.rs
@@ -22,4 +22,4 @@ impl Actor for StockEngine {
     type Context = Context<Self>;
 
     /// once started, perform ticking and update of stock data, and inform UserStore
-    fn started(&mut se
+    fn started(&mut self, _ctx: &mut 

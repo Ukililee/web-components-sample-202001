9962a736e7c36b3119e2979734b2bc7c7b1c6ae5
Ukililee <107114624+Ukililee@users.noreply.github.com>
2021-08-20 16:33:50 +0000

    Make it compile

--- api/src/actors/user_store.rs
+++ api/src/actors/user_store.rs
@@ -28,4 +28,4 @@ impl Handler<StockUpdated> for UserStore {
 
     /// on stock updates - iterate over all users and send them their subscribed prices
     /// also performs crediting the users
-    fn handle(&mut self, _msg: StockUpdated, _ctx: &mut Self::C
+    fn handle(&mut self, _msg: StockUpdated, _ctx: &mut Self::Context) -> Self

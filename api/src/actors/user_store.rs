8b3dfd755b99a24324406fe43d0024fb6f6dce7a
Ukililee <107114624+Ukililee@users.noreply.github.com>
2021-08-31 21:26:10 +0000

    block: avoid using uninitialized value in from queue_var_store

--- api/src/actors/user_store.rs
+++ api/src/actors/user_store.rs
@@ -34,4 +34,4 @@ impl Handler<StockUpdated> for UserStore {
         for (_, user) in &mut self.users {
             let subs = user.subscriptions.len() as u32;
 
-            if subs > 0 &
+            if subs > 0 && user.credits > 0 && 

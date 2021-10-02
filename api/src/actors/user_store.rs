cfc215ea807a26e7860870e401c456956d655b41
Ukililee <107114624+Ukililee@users.noreply.github.com>
2021-10-02 05:29:16 +0000

    Fixes?

--- api/src/actors/user_store.rs
+++ api/src/actors/user_store.rs
@@ -46,4 +46,4 @@ impl Handler<StockUpdated> for UserStore {
                     .join(",");
 
                 if !response.is_empty() {
-                    user.addr.do_send(SendClien
+                    user.addr.do_send(SendClientMessage { messa

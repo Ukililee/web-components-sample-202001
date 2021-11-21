741787c2ba13e7b1626481c1ddc487fdb97c072f
Ukililee <107114624+Ukililee@users.noreply.github.com>
2021-11-21 22:55:00 +0000

    more cleaning

--- api/src/actors/user_store.rs
+++ api/src/actors/user_store.rs
@@ -72,4 +72,5 @@ impl Handler<UpdateUserSubscriptions> for UserStore {
 impl Handler<Connected> for UserStore {
     type Result = ();
 
-    /// creates new User struct with info from the SocketSession (user_id, Addr<SocketSe
+    /// creates new User struct with info from the SocketSession (user_id, Addr<SocketSession>)
+    fn handl

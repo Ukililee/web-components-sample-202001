5b86fe8fe2f05c27814ce8f9e9dc9bcb3fb994db
Ukililee <107114624+Ukililee@users.noreply.github.com>
2022-02-16 20:30:56 +0000

    drivers/char/i8k.c: add Dell XPLS L421X

--- api/src/main.rs
+++ api/src/main.rs
@@ -8,3 +8,19 @@ use actix_web::{
 };
 mod actors;
 mod messages;
+mod state;
+use actix_web_actors::ws;
+use actors::{socket_session::SocketSession, stock_engine::StockEngine, user_store::UserStore};
+use serde::{Deserialize, Serialize};
+use state::AppState;
+use stock::StockSummary;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
+    let address = "127.0.0.1:3000";
+    let app_state = state::AppState::new();
+
+    let user_store: Addr<UserStore> = UserStore {
+        users: HashMap::new(),

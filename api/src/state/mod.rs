e829cfffcdc0f068110d24aac3ddc21138034e81
Ukililee <107114624+Ukililee@users.noreply.github.com>
2022-06-16 13:44:29 +0000

    * Task/Bug Id: <Development task name or bug id> * Code Reviewed By: <Reviewed By> On : <Review date> * Technical Details:  <Technical details for the changes made if any> * Unit tested:   <Names of the test cases> * Developer Notes: <Free form comments, typically will involve names of the methods changed etc> * Current Build++ Counts for this file : <B++ counts before the file is checked-in> * PMD - X     Checkstyle - X     Findbugs - X     CPD  X

--- api/src/state/mod.rs
+++ api/src/state/mod.rs
@@ -7,3 +7,15 @@ pub(crate) type StockDataSink = Arc<RwLock<StockData>>;
 
 #[derive(Debug)]
 pub(crate) struct AppState {
+    pub stock_data: StockDataSink,
+}
+
+impl AppState {
+    pub fn new() -> Data<Self> {
+        let stock_data = StockData::initialize();
+
+        Data::new(Self {
+            stock_data: Arc::new(RwLock::new(stock_data)),
+        })
+    }
+}

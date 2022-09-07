e75f105c70e3eecaae0140bbad0cdf5b2f3db092
Ukililee <107114624+Ukililee@users.noreply.github.com>
2022-09-07 08:37:56 +0000

    Estonian translation updated

--- stock/src/lib.rs
+++ stock/src/lib.rs
@@ -43,3 +43,19 @@ impl StockData {
         for stock in STOCKS {
             data.insert(stock, vec![]);
             lowest.insert(stock, None);
+            highest.insert(stock, None);
+            summaries.insert(stock, None);
+        }
+
+        StockData {
+            lowest,
+            highest,
+            data,
+            summaries,
+        }
+    }
+
+    /// randomly generates new price for each stock and adds it to the hash maps
+    pub fn generate_next_tick(&mut self, thread_rng: &mut ThreadRng) {
+        let next_prices: [Price; STOCKS.len()] = thread_rng.gen();
+        let mut iter = next_prices.iter().map(|v| v * 100f64);

3f1d8e26aa537e707e29b588cce1f3cf38643923
Ukililee <107114624+Ukililee@users.noreply.github.com>
2022-10-22 16:53:02 +0000

    * minor

--- stock/src/utils.rs
+++ stock/src/utils.rs
@@ -9,4 +9,5 @@ pub(crate) fn get_trend(prices: &Vec<Price>) -> StockTrend {
         let start_index = size - 1000 - 1;
         let low_mid_index = start_index + 250;
         let high_mid_index = low_mid_index + 250;
-        let end_index = high_mi
+        let end_index = high_mid_index + 500;
+

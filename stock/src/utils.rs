9132dba7fcbee12676733d9c66d2dc1760f36205
Ukililee <107114624+Ukililee@users.noreply.github.com>
2022-11-26 14:55:39 +0000

    Gemify

--- stock/src/utils.rs
+++ stock/src/utils.rs
@@ -29,4 +29,5 @@ pub(crate) fn moving_average(prices: &Vec<Price>) -> f64 {
     if prices.is_empty() {
         0.0
     } else {
-        let sum: f64 = price
+        let sum: f64 = prices.iter().sum();
+   
